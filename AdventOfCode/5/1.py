2bf2d9d06b2cbef65ddce6d0cdd75c4b4c73b81a
Kevin Ye <kevincye1996@gmail.com>
2024-06-17 19:25:48 -0700

    part 2 seems to be either complex or pointless, so I gave up

---
+++ AdventOfCode/5/1.py
@@ -0,0 +1,38 @@
+import os 
+
+def getMinLocation(seeds, range_list):
+    minLocation = 1000000000000
+    for s in seeds:
+        curNum = int(s)
+        for map_i in range(len(range_list)):
+            # for each map/range list, iterate over all tuples to see if the curNum is mapped by that tuple
+            for range_i in range(len(range_list[map_i])):
+                dest, src, length = range_list[map_i][range_i]
+                if curNum >= int(src) and curNum < int(src) + int(length):
+                    curNum -= (int(src) - int(dest))
+                    break
+        if curNum < minLocation:
+            minLocation = curNum
+    return minLocation
+
+if __name__ == "__main__":
+    f = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "input"))
+    # f = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "input_small"))
+
+    groups = f.read().split('\n\n')
+
+    range_list = [] # list of list of (int, int, int)
+    for i in range(len(groups) - 1):
+        range_list.append([])
+
+    seeds = groups[0].split(":")[1].strip().split(" ")
+
+    for map_i in range(len(range_list)):
+        lines = groups[map_i + 1].split('\n')
+        # omit first line
+        # add (dest src i) to a list for each map
+        for line_i in range(1, len(lines), 1):
+            range_list[map_i].append(lines[line_i].split(" "))
+    
+    # print (range_list)
+    print(getMinLocation(seeds, range_list))
